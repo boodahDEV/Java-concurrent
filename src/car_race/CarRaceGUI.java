fa64c504a9dad141c3530d5c39e4a337d64843a9
boodah <boodah21@protonmail.com>
2018-09-20 17:44:13 -0500

    Estable Car_Race.

--- src/car_race/CarRaceGUI.java
+++ src/car_race/CarRaceGUI.java
@@ -1,27 +1,11 @@
 package car_race;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.Color;
+import javax.swing.*;
 import javax.swing.border.MatteBorder;
 import utils.MaterialButton;
-import javax.swing.JButton;
-import java.awt.Cursor;
-import javax.swing.ImageIcon;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JLabel;
-import javax.swing.JSeparator;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import javax.swing.JTextField;
-import java.awt.Font;
-import javax.swing.SwingConstants;
+import java.awt.event.*;
+import java.util.Random;
+import java.awt.*;
 
 public class CarRaceGUI extends JFrame {
 
@@ -29,10 +13,11 @@ public class CarRaceGUI extends JFrame {
 	private JButton exit;
 	public int x,y;
 	private JLabel[] label = new JLabel[10];
-	private JTextField addcar;
+	private JTextField addcar,distrace;
 	private JLabel aviso;
-	private JTextField distrace;
-	private JSeparator separator_1;
+	private JSeparator separator1,separator,separator_1,separator_2;
+	private Random random = new Random();
+	private String[] nameCar = new String[10];
 	
 	/**
 	 * Launch the application.
@@ -42,8 +27,7 @@ public class CarRaceGUI extends JFrame {
 					CarRaceGUI frame = new CarRaceGUI();
 					frame.setVisible(true);
 					frame.setLocationRelativeTo(null);
-
-	}
+	}//end main
 
 	/**
 	 * Create the frame.
@@ -59,22 +43,21 @@ public class CarRaceGUI extends JFrame {
 		contentPane.setLayout(null);
 		
 		pista = new JPanel();
-		pista.setBackground(Color.WHITE);
+		//pista.setBackground(Color.WHITE);
 		pista.setBounds(10, 85, 377, 269);
 		contentPane.add(pista);
 		pista.setLayout(null);
-		
-//			label = new JLabel(" ");
-//			label.setIconTextGap(-1);
-//			label.setIcon(new ImageIcon(CarRaceGUI.class.getResource("/car_race/car_2.gif")));
-//			label.setBounds(10, 10, 45, 20);
-//			pista.add(label);
 			
 		meta = new JPanel();
 		meta.setBackground(Color.WHITE);
 		meta.setBounds(390, 85, 52, 269);
 		contentPane.add(meta);
 		meta.setLayout(null);
+		
+		separator_2 = new JSeparator();
+		separator_2.setOrientation(SwingConstants.VERTICAL);
+		separator_2.setBounds(50, 0, 2, 269);
+		meta.add(separator_2);
 		
 		display = new JPanel();
 		display.addMouseMotionListener(new MouseMotionAdapter() {
@@ -119,8 +102,18 @@ public class CarRaceGUI extends JFrame {
 		MaterialButton newcar = new MaterialButton();
 		newcar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent a) {
-				
-			}
+			try {
+				if(addcar.getText().isEmpty() == false  &&  Integer.parseInt(addcar.getText().trim())<=10 && Integer.parseInt(addcar.getText().trim())>0 ) {
+				nameCar[0]="Nissan";nameCar[1]="Buggati";nameCar[2]="Audi";nameCar[3]="BMW";nameCar[4]="Mercedes";nameCar[5]="Toyota";nameCar[6]="Subaru";nameCar[7]="Mustang";nameCar[8]="For";nameCar[9]="Chrebrolet";
+				aviso.setText("The threads were created.");aviso.setForeground(new Color(8,127,35));
+				newcar.setEnabled(false);addcar.setEnabled(false);
+				}else {
+					aviso.setText("Entry error, try again.");aviso.setForeground(new Color(220, 20, 60));
+				}
+			}catch(Exception e) {
+				aviso.setText("Entry error, try again.");aviso.setForeground(new Color(220, 20, 60));
+								}
+		   }
 		});
 		newcar.setText("Add Car");
 		newcar.setFocusable(false);
@@ -131,9 +124,14 @@ public class CarRaceGUI extends JFrame {
 		contentPane.add(newcar);
 		
 		MaterialButton vueltas = new MaterialButton();
+		vueltas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				vueltas.setEnabled(false);distrace.setEnabled(false);
+			}
+		});
 		vueltas.setText("Distance Race");
 		vueltas.setFocusable(false);
-		vueltas.setBounds(505, 147, 117, 35);
+		vueltas.setBounds(505, 149, 117, 35);
 		vueltas.setColorNormal(new Color(8,127,35));
 		vueltas.setColorHover(new Color(76,175,80));
 		vueltas.setColorPressed(new Color(118,210,117));
@@ -153,7 +151,7 @@ public class CarRaceGUI extends JFrame {
 		contentPane.add(separator);
 		
 		aviso = new JLabel("Los nombre se a\u00F1aden aleatoriamente");
-		aviso.setVisible(false);
+		aviso.setVisible(true);
 		aviso.setForeground(new Color(220, 20, 60));
 		aviso.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		aviso.setBounds(445, 122, 187, 14);
@@ -165,11 +163,53 @@ public class CarRaceGUI extends JFrame {
 		distrace.setFont(new Font("Century Gothic", Font.PLAIN, 20));
 		distrace.setColumns(10);
 		distrace.setBorder(null);
-		distrace.setBounds(450, 147, 45, 35);
+		distrace.setBounds(452, 147, 45, 35);
 		contentPane.add(distrace);
 		
+		separator1 = new JSeparator();
+		separator1.setBounds(452, 182, 45, 2);
+		contentPane.add(separator1);
+		
+		MaterialButton stop = new MaterialButton();
+		stop.setText("Stop");
+		stop.setFocusable(false);
+		stop.setBounds(521, 325, 52, 35);
+		stop.setColorNormal(new Color(8,127,35));
+		stop.setColorHover(new Color(76,175,80));
+		stop.setColorPressed(new Color(118,210,117));
+		contentPane.add(stop);
+		
+		MaterialButton star = new MaterialButton();
+		star.addActionListener(new ActionListener() {
+			int yy;
+			public void actionPerformed(ActionEvent a){
+			try {
+			  
+				  for(int i=0;i<Integer.parseInt(addcar.getText().trim());i++) {
+					  yy=i*(20-5);
+					label[i] = new JLabel(" ");
+					label[i].setIconTextGap(-1);
+					label[i].setIcon(new ImageIcon(CarRaceGUI.class.getResource("/car_race/car_2.gif")));
+					System.out.println(yy);
+					label[i].setBounds(0, yy, 45, 20);
+					label[i].setVisible(true);
+					pista.add(label[i]);
+				  new Car_GUI(nameCar[random.nextInt(10)], label[i],Integer.parseInt(distrace.getText().trim()),yy).start();System.out.println("se crea el hilo");
+			  }
+			}catch(Exception e) {}
+			}//action performet
+		});
+		star.setText("Run");
+		star.setFocusable(false);
+		star.setBounds(575, 325, 52, 35);
+		star.setColorNormal(new Color(8,127,35));
+		star.setColorHover(new Color(76,175,80));
+		star.setColorPressed(new Color(118,210,117));
+		contentPane.add(star);
+		
 		separator_1 = new JSeparator();
-		separator_1.setBounds(450, 182, 45, 2);
+		separator_1.setOrientation(SwingConstants.VERTICAL);
+		separator_1.setBounds(388, 85, 2, 269);
 		contentPane.add(separator_1);
 	}//end builder
 }//end class
---
+++ src/car_race/Car_GUI.java
@@ -0,0 +1,29 @@
+package car_race;
+
+import javax.swing.*;
+
+public class Car_GUI extends Thread {
+	String nombre;
+	JLabel label;
+	int finish, yy;
+	public Car_GUI(String nombre, JLabel label, int finish, int yy) {
+		this.nombre=nombre;
+		this.label=label;
+		this.finish=finish;
+	}
+	
+	public void run() {
+		int time = (int) (Math.random() * 5000);
+		
+		for(int i=1;i<=finish;i++) {
+			
+			try {
+				Thread.sleep(time);
+			}catch(Exception e) {}
+			label.setBounds(0+(i*50), yy, 45, 20);
+			
+			System.out.println(nombre + " -> "+i);
+		}//end for
+		System.out.println(nombre + " finished!"+"\n");
+	}
+}//end class
